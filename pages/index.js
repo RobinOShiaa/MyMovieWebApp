72f34c2f19574edf27a2bae04ae2467d5c322892
RobinOShiaa <oshea.robinho@gmail.com>
2023-05-22 14:26:53 +0100

    css adjustments

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,7 @@ import { getPopularMovies } from "@/src/HttpLibrary";
 import { useEffect, useState } from "react";
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-
-
+import Link from 'next/link'
 export default function Home() {
   const [state,setState] = useState([]);
   getPopularMovies(1,setState);
@@ -16,14 +14,12 @@ export default function Home() {
 
 
 
+
   
   return (
     <>
         <div className="h-screen w-screen bg-gradient-to-r from-slate-800 to-purple-900">
-          <nav className="flex w-full justify-around h-24 text-zinc-600 bg-slate-200">
-            <div className="ml-10 p-6 text-3xl my-2 font-bold">
-              <a href="#">Logo</a>
-            </div>
+          <nav className="absolute flex w-full justify-end z-20 h-24 text-white">
             <div className={styles.main_list}>
               <ul className="flex w-1/2 whitespace-nowrap">
                 <li className="mx-6 text-xl border-b-4 border-indigo-500 hover:text-violet-700 rounded-lg  border-double  h-20 p-7 my-2">
@@ -36,26 +32,33 @@ export default function Home() {
                   <a href="#">Latest</a>
                 </li>
                 <li className="mx-6 text-xl border-b-4 border-indigo-500 hover:text-violet-700 rounded-lg  border-double  h-20 p-7 my-2">
-                  <a href="#">WatchList</a>
+                  <a href="#">Genre</a>
+                </li>
+                <li className="mx-6 text-xl border-b-4 border-indigo-500 hover:text-violet-700 rounded-lg  border-double  h-20 p-7 my-2">
+                  <a href="#">Country</a>
+                </li>
+                <li className="mx-6 text-xl border-b-4 border-indigo-500 hover:text-violet-700 rounded-lg  border-double  h-20 p-7 my-2">
+                  <a href="#">Year</a>
                 </li>
               </ul>
             </div>
           </nav>
           <main>
-              <Carousel showStatus={false} showThumbs={false} autoPlay emulateTouch={true}>
-                {state.map((item,idx) =>  (
-                  <div key={idx} className="relative">
-                    <div className="absolute flex flex-col w-full text-start mt-8 ml-10">
-                      <h1 className="text-white text-6xl ">{item.original_title}</h1>
-                      <p className="text-white text-justify w-1/3">{item.overview}</p>
-                      <p className=" text-white">{item.release_date}</p>
-                      <p className=" text-white">{item.vote_average}</p>
-                    </div>
-                    <img src={'https://www.themoviedb.org/t/p/original' + item['backdrop_path']} className="h-128 mx-auto"/>
-                  </div>))}     
-              </Carousel>
-         
-               
+                <Carousel showStatus={false} showThumbs={false} autoPlay emulateTouch={true}>
+                  {state.map((item,idx) =>  (
+                    <div key={idx} className="relative">
+                      <div className="absolute flex gap-8 flex-col w-full text-start my-40 ml-10 text-white z-10">
+                        <h1 className=" text-6xl ">{item.original_title}</h1>
+                        <p className="text-justify w-1/3">{item.overview}</p>
+                        <p>Release Date: {item.release_date}</p>
+                        <p>Rating: {item.vote_average}</p>
+                        <Link href={`https://hd.fmoviesto.site/search/?q=${item.title}`}>
+                          <span className=" text-zinc-600 bg-white p-3"> Watch Now </span>
+                        </Link>
+                      </div>
+                      <img src={'https://www.themoviedb.org/t/p/original' + item['backdrop_path']} className="h-128 opacity-50"/>
+                    </div>))}     
+                </Carousel>
           </main>
         </div>
     
