0399822c26880bb32fb0e3a14d73779e61a761ba
RobinOShiaa <oshea.robinho@gmail.com>
2023-05-22 13:23:05 +0100

    now using new react-carousel dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing : {
+        '128' : '43rem',
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
