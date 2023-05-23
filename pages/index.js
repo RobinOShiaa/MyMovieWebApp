9b0b5ddc8bea67a6befd531b33cb1c1813e2cd5c
RobinOShiaa <oshea.robinho@gmail.com>
2023-05-23 17:03:00 +0100

    further improvement and change in design of home page

--- pages/index.js
+++ pages/index.js
@@ -3,64 +3,231 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import { getPopularMovies, getTrendingMovies } from "@/src/HttpLibrary";
 import { useEffect, useState } from "react";
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Link from 'next/link'
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Link from "next/link";
 export default function Home() {
-  const [state,setState] = useState([]);
-  const [trending,setTrending] = useState([]);
+  const [state, setState] = useState([]);
+  const [isHover, setIsHover] = useState({
+    movies: false,
+    tvshows: false,
+    latest: false,
+    genre: false,
+    country: false,
+    year: false,
+  });
 
+  const borderStyle = "border-b-4 rounded-md border-indigo-500 border-single";
+  const navStyle = "2xl:mx-2 md:text-xl text-lg h-20 2xl:p-7 xl:p-6 lg:p-5 md-p4 sm:p3 p-1 my-2 hover:font-bold";
 
   useEffect(() => {
-    getPopularMovies(1,setState);
-    getTrendingMovies(1,setTrending);
-  },[])
+    getPopularMovies(1, setState);
+    // getTrendingMovies(1,setTrending);
+  }, []);
 
-  
   return (
     <>
-        <div className="h-screen w-screen bg-gradient-to-r from-slate-800 to-purple-900">
-          <nav className="absolute flex w-full justify-end z-20 h-24 text-white">
-            <div className={styles.main_list}>
-              <ul className="flex w-1/2 whitespace-nowrap mr-16">
-                <li className="mx-3 text-xl hover:text-violet-700 rounded-lg  h-20 p-7 my-2">
-                  <a href="#">Movies</a>
-                </li>
-                <li className="mx-3 text-xl  hover:text-violet-700 rounded-lg h-20 p-7 my-2">
-                  <a href="#">TV Shows</a>
-                </li>
-                <li className="mx-3 text-xl  hover:text-violet-700 rounded-lg h-20 p-7 my-2">
-                  <a href="#">Latest</a>
-                </li>
-                <li className="mx-3 text-xl  hover:text-violet-700 rounded-lg h-20 p-7 my-2">
-                  <a href="#">Genre</a>
-                </li>
-                <li className="mx-3 text-xl  hover:text-violet-700 rounded-lg h-20 p-7 my-2">
-                  <a href="#">Country</a>
-                </li>
-                <li className="mx-3 text-xl hover:text-violet-700 rounded-lg h-20 p-7 my-2">
-                  <a href="#">Year</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <main>
-                <Carousel infiniteLoop={true} showArrows={false} showStatus={false} showIndicators={false} showThumbs={false} autoPlay emulateTouch={true}>
-                  {state.map((item,idx) =>  (
-                    <div key={idx} className="relative [text-shadow:_0_4px_0_rgb(0_0_0_/_40%)]">
-                      <div className="absolute flex gap-8 flex-col w-full text-start my-40 ml-10 text-white z-10">
-                        <h1 className="font-stargaze text-6xl ">{item.original_title}</h1>
-                        <p className="font-titillium text-justify w-1/3">{item.overview}</p>
-                        <p className="font-orbitron text-lg">Release Date: {item.release_date}</p>
-                        <p className="font-orbitron text-lg">Rating: {item.vote_average}</p>
-                        {/* <Link href={`https://hd.fmoviesto.site/search/?q=${item.title}`}>
+      <div className="h-screen bg-black">
+        <nav className="absolute flex w-full md:justify-end z-20 h-24 text-white justify-center">
+          <div className={styles.main_list}>
+            <ul className="flex  whitespace-nowrap md:mr-16">
+              <li
+                onMouseEnter={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      movies: true,
+                    };
+                    console.log(result);
+                    return result;
+                  });
+                }}
+                onMouseLeave={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      movies: false,
+                    };
+                    console.log(result);
+                    return result;
+                  });
+                }}
+                className={isHover.movies ? borderStyle + navStyle : navStyle}
+              >
+                <a href="#">Movies</a>
+              </li>
+              <li
+                onMouseEnter={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      tvshows: true,
+                    };
+                    console.log(result);
+                    return result;
+                  });
+                }}
+                onMouseLeave={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      tvshows: false,
+                    };
+                    console.log(result);
+                    return result;
+                  });
+                }}
+                className={isHover.tvshows ? borderStyle + navStyle : navStyle}
+              >
+                <a href="#">TV Shows</a>
+              </li>
+              <li
+                onMouseEnter={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      latest: true,
+                    };
+                    console.log(result);
+                    return result;
+                  });
+                }}
+                onMouseLeave={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      latest: false,
+                    };
+                    console.log(result); 
+                    return result;
+                  });
+                }}
+                className={isHover.latest ? borderStyle + navStyle : navStyle}
+              >
+                <a href="#">Latest</a>
+              </li>
+              <li
+                onMouseEnter={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      genre: true,
+                    };
+                    console.log(result);
+                    return result;
+                  });
+                }}
+                onMouseLeave={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      genre: false,
+                    };
+                    console.log(result);
+                    return result;
+                  });
+                }}
+                className={isHover.genre ? borderStyle + navStyle : navStyle}
+              >
+                <a href="#">Genre</a>
+              </li>
+              <li
+                onMouseEnter={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      country: true,
+                    };
+                    console.log(result); 
+                    return result;
+                  });
+                }}
+                onMouseLeave={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      country: false,
+                    };
+                    console.log(result); 
+                    return result;
+                  });
+                }}
+                className={isHover.country ? borderStyle + navStyle : navStyle}
+              >
+                <a href="#">Country</a>
+              </li>
+              <li
+                onMouseEnter={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      year: true,
+                    };
+                    console.log(result); 
+                    return result;
+                  });
+                }}
+                onMouseLeave={() => {
+                  setIsHover((prevState) => {
+                    let result = {
+                      ...prevState,
+                      year: false,
+                    };
+                    console.log(result);
+                    return result;
+                  });
+                }}
+                className={isHover.year ? borderStyle + navStyle : navStyle}
+              >
+                <a href="#">Year</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <main>
+          <Carousel
+            infiniteLoop={true}
+            showArrows={false}
+            showStatus={false}
+            showIndicators={false}
+            showThumbs={false}
+            width={'100%'}
+            autoPlay
+            emulateTouch={true}
+          >
+            {state.map((item, idx) => (
+              <div key={idx} className="relative">
+                <div className="absolute h-full md:items-start items-center justify-center flex gap-8 flex-col text-start ml-10 text-white z-10">
+                  <h1 className="font-stargaze text-2xl text-center sm:text-3xl lg:text-4xl xl:text-5xl [text-shadow:_4px_0_rgb(0_0_0_/_50%)] ">
+                    {item.original_title}
+                  </h1>
+                  <p className="font-titillium text-md xl:text-lg w-3/4 [text-shadow:_4px_0_rgb(0_0_0_/_40%)] ">
+                    {item.overview}
+                  </p>
+                  <p className="font-orbitron text-sm xl:text-md [text-shadow:_1px_0_rgb(0_0_0_/_40%)]">
+                    Release Date: {item.release_date}
+                  </p>
+                  <p className="font-orbitron text-sm xl:text-md [text-shadow:_1px_0_rgb(0_0_0_/_40%)]">
+                    Rating: {item.vote_average}
+                  </p>
+                  {/* <Link href={`https://hd.fmoviesto.site/search/?q=${item.title}`}>
                           <span className=" text-zinc-600 bg-white p-3"> Watch Now </span>
+                        </Link>
+                        <Link href={`https://hd.fmoviesto.site/search/?q=${item.title}`}>
+                          <span className=" text-zinc-600 bg-white p-3"> Tell Me More </span>
                         </Link> */}
-                      </div>
-                      <img src={'https://www.themoviedb.org/t/p/original' + item['backdrop_path']} className="h-128 opacity-60"/>
-                    </div>))}     
-                </Carousel>
-                <h3 className="text-xl text-white text-center border-b-4 rounded-lg border-indigo-500 border-double"></h3>
+                </div>
+                <img
+                  src={
+                    "https://www.themoviedb.org/t/p/original" +
+                    item["backdrop_path"]
+                  }
+                  className=" h-screen w-full opacity-70"
+                />
+              </div>
+            ))}
+          </Carousel>
+          {/* <h3 className="text-xl text-white text-center border-b-4 rounded-lg border-indigo-500 border-double"></h3>
                 <div className="mx-8 grid grid-cols-8 gap-4 p-4 m-4">
                     {trending.map((item,idx) =>  (
                       <div key={idx}>
@@ -70,10 +237,10 @@ export default function Home() {
                         <h4 className="text-center text-ellipsis overflow-hidden whitespace-nowrap text-white">{item.title}</h4>
                       </div>
                     ))}  
-                </div>
-          </main>
-        </div>
+                </div> */}
+            {/* <div className="bg-gradient-to-r from-slate-800 to-purple-900  h-screen"></div> */}
+        </main>
+      </div>
     </>
-    
   );
 }
